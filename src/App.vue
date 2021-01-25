ee4e8b1bd7d7e2570b88b5c3b5aa5d338cd33020
henryhansollee <henryhansollee@gmail.com>
2021-01-25 14:25:31 +0900

    Add coming soon page

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="d-flex justify-content-center">
+    <div class="d-flex flex-column align-items-center">
+      <h1 class="main-h1">밍순밍순 커밍순</h1>
+      <a class="btn btn-dark mt-3" href="https://frosty-einstein-bf8978.netlify.app/">돌아가기</a>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.main-h1 {
+  margin-top: 100%;
 }
 </style>
