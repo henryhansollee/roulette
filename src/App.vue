4915ed2e55d20755ce005ae465ecd79e0f0e3f55
henryhansollee <henryhansollee@gmail.com>
2021-01-25 15:00:15 +0900

    Change font

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app" class="d-flex justify-content-center">
     <div class="d-flex flex-column align-items-center">
       <h1 class="main-h1">밍순밍순 커밍순</h1>
-      <a class="btn btn-dark mt-3" href="https://frosty-einstein-bf8978.netlify.app/">돌아가기</a>
+      <a class="btn btn-dark mt-3 main-button" href="https://frosty-einstein-bf8978.netlify.app/">돌아가기</a>
     </div>
   </div>
 </template>
@@ -16,5 +16,15 @@ export default {
 <style>
 .main-h1 {
   margin-top: 100%;
+  font-family: 'CookieRunOTF-Bold';
+}
+.main-button {
+  font-family: 'CookieRunOTF-Bold';
+}
+@font-face {
+  font-family: 'CookieRunOTF-Bold';
+  src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_twelve@1.0/CookieRunOTF-Bold00.woff') format('woff');
+  font-weight: normal;
+  font-style: normal;
 }
 </style>
